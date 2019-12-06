d9124f983abddb18fdea23f3479a054ce098bab1
WuGuichen <577024128@qq.com>
2019-12-07 02:51:47 +0800

    往手上自动组装WC

--- Scripts/WeaponManager.cs
+++ Scripts/WeaponManager.cs
@@ -11,13 +11,30 @@ public class WeaponManager : IActorManagerInterface
     public GameObject whL;
     public GameObject whR;
 
+    public WeaponController wcL;
+    public WeaponController wcR;
+
     private void Start()
     {
         whL = transform.DeepFind("weaponHandleL").gameObject;
         whR = transform.DeepFind("weaponHandleR").gameObject;
 
+        wcL = BindWeaponController(whL);
+        wcR = BindWeaponController(whR);
+
         weaponColL = whL.GetComponentInChildren<Collider>();
         weaponColR = whR.GetComponentInChildren<Collider>();
+    }
+
+    public WeaponController BindWeaponController(GameObject targetObj)
+    {
+        WeaponController tempWc;
+        tempWc = targetObj.GetComponent<WeaponController>();
+        if (tempWc == null)
+            tempWc = targetObj.AddComponent<WeaponController>();
+        tempWc.wm = this;
+
+        return tempWc;
     }
 
     public void WeaponEnable()
