a72508f019028787796ec29d553d9b6faeaeb878
WuGuichen <577024128@qq.com>
2019-12-10 02:33:25 +0800

    代码测试

---
+++ TexterDirector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TexterDirector : MonoBehaviour
+{
+    public PlayableDirector pd;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            pd.time = 0;
+            pd.Stop();
+            pd.Evaluate();
+            pd.Play();
+        }
+    }
+}
