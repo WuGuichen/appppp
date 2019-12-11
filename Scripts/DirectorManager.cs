f5f14636775c47ec868bfb586d02b389eab94051
WuGuichen <577024128@qq.com>
2019-12-12 02:13:01 +0800

    让pd工作——1

--- Scripts/DirectorManager.cs
+++ Scripts/DirectorManager.cs
@@ -57,25 +57,42 @@ public class DirectorManager : IActorManagerInterface
         {
             pd.playableAsset = Instantiate(frontStab);
 
-            foreach (var track in pd.playableAsset.outputs)
+            //取到第一层级Timeline
+            TimelineAsset timeline = (TimelineAsset)pd.playableAsset;
+
+            //取track
+            foreach (var track in timeline.GetOutputTracks())
             {
-                if (track.streamName == "Attacker Script")
-                {
-                    pd.SetGenericBinding(track.sourceObject, attacker);
-                }
-                else if (track.streamName == "Victim Script")
-                {
-                    pd.SetGenericBinding(track.sourceObject, victim);
-                }
-                else if (track.streamName == "Attacker Animation")
-                {
-                    pd.SetGenericBinding(track.sourceObject, attacker.ac.anim);
-                }
-                else if (track.streamName == "Victim Animation")
-                {
-                    pd.SetGenericBinding(track.sourceObject, victim.ac.anim);
-                }
+                if (track.name == "Attacker Script")
+                    pd.SetGenericBinding(track, attacker);
+                else if (track.name == "Victim Script")
+                    pd.SetGenericBinding(track, victim);
+                else if (track.name == "Attacker Animation")
+                    pd.SetGenericBinding(track, attacker.ac.anim);
+                else if (track.name == "Victim Animation")
+                    pd.SetGenericBinding(track, victim.ac.anim);
+
             }
+
+            //foreach (var trackBinding in pd.playableAsset.outputs)
+            //{
+            //    if (trackBinding.streamName == "Attacker Script")
+            //    {
+            //        pd.SetGenericBinding(trackBinding.sourceObject, attacker);
+            //    }
+            //    else if (trackBinding.streamName == "Victim Script")
+            //    {
+            //        pd.SetGenericBinding(trackBinding.sourceObject, victim);
+            //    }
+            //    else if (trackBinding.streamName == "Attacker Animation")
+            //    {
+            //        pd.SetGenericBinding(trackBinding.sourceObject, attacker.ac.anim);
+            //    }
+            //    else if (trackBinding.streamName == "Victim Animation")
+            //    {
+            //        pd.SetGenericBinding(trackBinding.sourceObject, victim.ac.anim);
+            //    }
+            //}
             pd.Play();
         }
     }
