b6b59803b663146723c09164e77e7537b5c7e319
WuGuichen <577024128@qq.com>
2019-12-13 21:55:41 +0800

    加入GameObject和Resources

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// Make it a singleton....later
+    /// </summary>
+
+
+    
+    void Start()
+    {
+        GameObject prefab = (GameObject) Resources.Load("Falchion");
+        Instantiate(prefab, Vector3.zero, Quaternion.identity);
+
+        //Resources.UnloadUnusedAssets(); // not necessary
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
